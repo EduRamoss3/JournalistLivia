9db842d16fa7782b0fe10082c42806b5a217db22
Eduardo Ramos <eduardoramossilva@live.com>
2025-07-20 14:14:00 -0300

    feature

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ let startX = 0;
 let endX = 0;
 let interval = 3200;
 let direction = 1;
-
+let max = 600;
 slider.addEventListener('touchstart',(e) => {
     startX = e.touches[0].clientX;
 });
@@ -19,33 +19,33 @@ slider.addEventListener('touchend', (e) => {
         if (diffX < 0) {
             // arrastou para a esquerda
             direction = -1;
-            intervalMund -= 170;
+            intervalMund -= 200;
         } else {
             // arrastou para a direita
             direction = 1;
-            intervalMund += 170;
+            intervalMund += 200;
         }
 
         // Limita dentro dos valores permitidos
-        if (intervalMund >= 510) intervalMund = 510;
-        if (intervalMund < -510) intervalMund = -510;
+        if (intervalMund >= max) intervalMund = max;
+        if (intervalMund < -max) intervalMund = -max;
 
         slider.style.transform = `translate3d(${intervalMund}px, 0px, 0px)`;
-        slider.style.transition = 'transform 300ms ease-in-out';
+        slider.style.transition = 'transform 300ms';
     }
 })
 
 function carousel() {
-    intervalMund += 170 * direction;
+    intervalMund += 200 * direction;
 
-    if (intervalMund >= 510) 
+    if (intervalMund >= max) 
     {
-        intervalMund = 510;
+        intervalMund = max;
         direction = -1;
     }
-    if(intervalMund <= -510)
+    if(intervalMund <= -max)
     {
-        intervalMund = -510;
+        intervalMund = -max;
         direction = 1;
     }
 
